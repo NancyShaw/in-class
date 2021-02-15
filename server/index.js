d7651157edaa6da4ed496795f52e692825c031ac
Nancy Shaw <78286574+NancyShaw@users.noreply.github.com>
2021-02-15 16:29:16 -0500

    Code example notes from class -adding package.json, including some dependencies, nodemon as a devDependency, a couple of scripts -adding a server folder -creating simple node.js server example

---
+++ server/index.js
@@ -0,0 +1,16 @@
+//example from Node.js
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Universe!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
